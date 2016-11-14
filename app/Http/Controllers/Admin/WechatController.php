c603b38e77436e1f31f231da24a7421d2bf972c8
zhonglong <long.zhong@timelink.cn>
2016-11-14 17:05:16 +0800

    wechat login test

--- app/Http/Controllers/Admin/WechatController.php
+++ app/Http/Controllers/Admin/WechatController.php
@@ -21,7 +21,7 @@ class WechatController extends Controller
     public function loginAction(Request $request){
         $redirectURL = 'http%3A%2F%2Fattainment.timelink.cn%2FWechatLogin';
         $urlCode = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$this->WechatInfo['WECHAT_APPID']
-            .'&redirect_uri='.$redirectURL.'&response_type=code&scope=snsapi_base&state=1#wechat_redirect';
+            .'&redirect_uri='.$redirectURL.'&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
         $code = $request->get('code');
         redirect($urlCode);
         if(empty($code)){
@@ -42,10 +42,9 @@ class WechatController extends Controller
         if(!empty($access_token)){
             $token_info = json_decode($access_token,true);
             dd($token_info);
-            $unionid = $token_info['unionid'];
             $openid = $token_info['openid'];
 
-            $user = User::where("unionid",$unionid)->first();
+            $user = User::where("openid",$openid)->first();
             if(!empty($user)){
                 Session::set('userId',$user->id);
                 response(redirect('/wall'));
