150f331fabe16117f0bf6510953e601e03d3c3bc
zhonglong <long.zhong@timelink.cn>
2016-11-14 17:02:27 +0800

    wechat login test

--- app/Http/Controllers/Admin/WechatController.php
+++ app/Http/Controllers/Admin/WechatController.php
@@ -41,6 +41,7 @@ class WechatController extends Controller
     private function wechat($access_token,$request){
         if(!empty($access_token)){
             $token_info = json_decode($access_token,true);
+            dd($token_info);
             $unionid = $token_info['unionid'];
             $openid = $token_info['openid'];
 
