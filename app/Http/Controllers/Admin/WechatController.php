641055af612f782941c0f927a659028a8e786524
zhonglong <long.zhong@timelink.cn>
2016-11-14 15:55:45 +0800

    wechat login test

--- app/Http/Controllers/Admin/WechatController.php
+++ app/Http/Controllers/Admin/WechatController.php
@@ -20,7 +20,7 @@ class WechatController extends Controller
      * @param Request $request
      */
     public function loginAction(Request $request){
-        $redirectURL = 'http%3A%2F%2Fattainment.timelink.cn%2Flogin';
+        $redirectURL = 'http%3A%2F%2Fattainment.timelink.cn%2Fget_code';
         if(empty($request->get('code')))
         {
             $urlCode = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$this->WechatInfo['WECHAT_APPID']
