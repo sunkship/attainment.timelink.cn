3cb51b85cdce987582ec24100f948fe0c5d94775
zhonglong <long.zhong@timelink.cn>
2016-11-15 11:15:17 +0800

    test redirect page

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,6 +21,7 @@ class Authenticate
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
             } else {
+                dd('login');
                 return redirect()->guest('login');
             }
         }
